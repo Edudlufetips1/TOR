8dfe1208d91c750e088c0417b6806e9ddb8fc2f3
Edudlufetips1 <aq8829@outlook.com>
2026-05-03 10:30:04 -0400

    formatted at_bat, fixed bugs in ab methods

--- at_bat.py
+++ at_bat.py
@@ -4,11 +4,12 @@ class Pitch:
         self.exit_velo = exit_velo
         self.description = description
 
+
 class AtBat:
     def __init__(self, player_name):
         self.player_name = player_name
         self.pitches = []
-    
+
     def add_pitch(self, pitch):
         self.pitches.append(pitch)
 
@@ -18,27 +19,29 @@ class AtBat:
             return 2
         elif n >= 4:
             return 1
-        elif n ==3 :
+        elif n == 3:
             return 0
         else:
             return -1
 
     def quality_contact_score(self):
-       score = 0     
-       for pitch in self.pitches:
-            if pitch.description == "foul" or pitch.description == "in_play":
+        score = 0
+        for pitch in self.pitches:
+            if pitch.description in ("foul", "in_play"):
                 if pitch.exit_velo > 105:
                     score += 3
                 elif pitch.exit_velo > 95:
                     score += 1.5
                 elif pitch.exit_velo < 80:
-                    score += -1
+                    score -= 1
         return score
-    
+
+    def count_management_score(self):
+        return 0  # placeholder so Python doesn't crash
+
     def calculate_tor(self):
-        score = 50 
+        score = 50
         score += self.pitch_count_score()
         score += self.quality_contact_score()
         score += self.count_management_score()
         return score
-        
